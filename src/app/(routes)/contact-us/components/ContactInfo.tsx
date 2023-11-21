1f2baf551d9ff5296f9e80fa714ae57b0481e7b5
Dobromir Stefanov Ivanov <lalilulelo.mg@gmail.com>
2023-11-22 05:49:14 +1100

    added new contacts number

--- src/app/(routes)/contact-us/components/ContactInfo.tsx
+++ src/app/(routes)/contact-us/components/ContactInfo.tsx
@@ -8,7 +8,7 @@ export default function ContactInfo() {
   //TODO : change number location email everything
 return (
   <div className="flex-1 px-4 space-y-4 sm:space-y-10 flex flex-col justify-center ">
-     <InfoBlock icon={<CallSVG fill="#FF5722"  className="w-10 sm:w-20"/>} textInfo="00000 add num here" />
+     <InfoBlock icon={<CallSVG fill="#FF5722"  className="w-10 sm:w-20"/>} textInfo="+359 898 626 869" />
      <InfoBlock icon={<MailSVG  fill="#FF5722" className="p-1 sm:p-3 w-10 sm:w-20"/>} textInfo="motorevive.bg@gmail.com" />
      <InfoBlock icon={<LocationSVG  fill="#FF5722" className="p-1 sm:p-3 w-10 sm:w-20 "/>} textInfo="Александър add str here" />
   </div>
